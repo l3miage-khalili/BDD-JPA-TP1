5e56bd5f78bda0fd1d13e40578843c2ae2c1982e
l3miage-khalili <khalilbrahimgoukouni@gmail.com>
2024-03-10 02:43:36 +0100

    réalise partie 1 et début partie 2 - génère des erreurs à l'étape 2.3.1

---
+++ cr.md
@@ -0,0 +1,6 @@
+    Compte rendu TP1
+    Created by KHALIL IBRAHIM
+
+    Exo1
+Une question : En lançant la commande mvn test à l'étape 1.7 de l'exo1, je tombe sur cette erreur et j'aimerais comprendre le problème associé :
+![img.png](img.png)
---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
@@ -0,0 +1,35 @@
+package fr.uga.l3miage.tp1.exo1.models;
+
+import fr.uga.l3miage.tp1.exo1.enums.CylinderNumber;
+import fr.uga.l3miage.tp1.exo1.enums.ShifterType;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bike_triumph")
+public class BikeEntity {
+    @Id
+    @Column(name = "immat")
+    private String immatriculation ;
+
+    @Column(name = "capacity")
+    private Integer capacity;
+
+    @Column(name = "cylinder_number")
+    @Enumerated(EnumType.STRING)
+    private CylinderNumber cylinderNumber ;
+
+    @Column(name = "automatic")
+    private Boolean automatic ;
+
+    @Column(name = "shifter")
+    private Boolean shifter ;
+
+    @Column(name = "shifter_type")
+    @Enumerated(EnumType.ORDINAL)
+    private ShifterType shifterType ;
+
+    @Column(name = "circulation_date")
+    private LocalDate circulationDate ;
+}
